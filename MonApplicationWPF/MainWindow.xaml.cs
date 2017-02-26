c1070d8b137379c6b48e0dd6bd2d8f7562c0202b
Milou <Milou@DESKTOP-R3P5NON>
2017-02-26 15:27:21 +0100

    MaWPF

--- MonApplicationWPF/MainWindow.xaml.cs
+++ MonApplicationWPF/MainWindow.xaml.cs
@@ -18,11 +18,21 @@ namespace MonApplicationWPF
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
+
+
+        /*Un Class "MainWindow" dérivant d'une class "Window" 
+         appartenant au NameSpace "MonApplicationWPF"*/
     public partial class MainWindow : Window
     {
+
+        /*constructeur pour la fenêtre */
         public MainWindow()
         {
+            /*methode heritée de la class "Window" nous permettant d'initialiser 
+             tous les composants graphique de la class "MainWindow" */
             InitializeComponent();
+            MonPremierLabel.Content = "contenu modifier";
+            MonSecondLabel.Content = "Contenu Modifié une second fois";
         }
     }
 }
